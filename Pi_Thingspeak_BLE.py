2c15cab226955c1f550f9802e60fcdbefeb10b51
GayathriBoopathyUML <Gayathri_Boopathy@student.uml.edu>
2023-10-01 12:55:55 -0400

    BLE send data to app

--- Pi_Thingspeak_BLE.py
+++ Pi_Thingspeak_BLE.py
@@ -1,33 +1,207 @@
-import machine
-import time
-import ujson
+# This example demonstrates a UART periperhal.
+
 import bluetooth
+import random
+import struct
+import time
+from machine import Pin, ADC
+from micropython import const
 
-# Configure the onboard temperature sensor
-temp_sensor = machine.ADC(machine.ADC.CORE_TEMP)
 
 
-while True:
-    # Read the temperature sensor value
-    sensor_value = temp_sensor.read_u16() * (3.3 / (65536))
+_ADV_TYPE_FLAGS = const(0x01)
+_ADV_TYPE_NAME = const(0x09)
+_ADV_TYPE_UUID16_COMPLETE = const(0x3)
+_ADV_TYPE_UUID32_COMPLETE = const(0x5)
+_ADV_TYPE_UUID128_COMPLETE = const(0x7)
+_ADV_TYPE_UUID16_MORE = const(0x2)
+_ADV_TYPE_UUID32_MORE = const(0x4)
+_ADV_TYPE_UUID128_MORE = const(0x6)
+_ADV_TYPE_APPEARANCE = const(0x19)
 
-    # Convert the sensor value to temperature in degrees Celsius
-    temperature_celsius = 27 - (sensor_value - 0.706)/0.001721
 
-    # Convert Celsius to Fahrenheit
-    temperature_fahrenheit = (temperature_celsius * 9/5) + 32
+# Generate a payload to be passed to gap_advertise(adv_data=...).
+def advertising_payload(limited_disc=False, br_edr=False, name=None, services=None, appearance=0):
+    payload = bytearray()
 
-    # Print the temperature in both Celsius and Fahrenheit
-    print("Temperature in C: {:.2f}°C".format(temperature_celsius))
-    print("Temperature in F: {:.2f}°F".format(temperature_fahrenheit))
-    print("<...>")
-    time.sleep_ms(500)
+    def _append(adv_type, value):
+        nonlocal payload
+        payload += struct.pack("BB", len(value) + 1, adv_type) + value
+
+    _append(
+        _ADV_TYPE_FLAGS,
+        struct.pack("B", (0x01 if limited_disc else 0x02) + (0x18 if br_edr else 0x04)),
+    )
+
+    if name:
+        _append(_ADV_TYPE_NAME, name)
+
+    if services:
+        for uuid in services:
+            b = bytes(uuid)
+            if len(b) == 2:
+                _append(_ADV_TYPE_UUID16_COMPLETE, b)
+            elif len(b) == 4:
+                _append(_ADV_TYPE_UUID32_COMPLETE, b)
+            elif len(b) == 16:
+                _append(_ADV_TYPE_UUID128_COMPLETE, b)
+
+    if appearance:
+        _append(_ADV_TYPE_APPEARANCE, struct.pack("<h", appearance))
+
+    return payload
+
+
+def decode_field(payload, adv_type):
+    i = 0
+    result = []
+    while i + 1 < len(payload):
+        if payload[i + 1] == adv_type:
+            result.append(payload[i + 2 : i + payload[i] + 1])
+        i += 1 + payload[i]
+    return result
+
+
+def decode_name(payload):
+    n = decode_field(payload, _ADV_TYPE_NAME)
+    return str(n[0], "utf-8") if n else ""
+
+
+def decode_services(payload):
+    services = []
+    for u in decode_field(payload, _ADV_TYPE_UUID16_COMPLETE):
+        services.append(bluetooth.UUID(struct.unpack("<h", u)[0]))
+    for u in decode_field(payload, _ADV_TYPE_UUID32_COMPLETE):
+        services.append(bluetooth.UUID(struct.unpack("<d", u)[0]))
+    for u in decode_field(payload, _ADV_TYPE_UUID128_COMPLETE):
+        services.append(bluetooth.UUID(u))
+    return services
+
+
+
+_IRQ_CENTRAL_CONNECT = const(1)
+_IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_WRITE = const(3)
+
+_FLAG_READ = const(0x0002)
+_FLAG_WRITE_NO_RESPONSE = const(0x0004)
+_FLAG_WRITE = const(0x0008)
+_FLAG_NOTIFY = const(0x0010)
+
+_UART_UUID = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
+_UART_TX = (
+    bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"),
+    _FLAG_READ | _FLAG_NOTIFY,
+)
+_UART_RX = (
+    bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"),
+    _FLAG_WRITE | _FLAG_WRITE_NO_RESPONSE,
+)
+_UART_SERVICE = (
+    _UART_UUID,
+    (_UART_TX, _UART_RX),
+)
+
+
+class BLESimplePeripheral:
+    def __init__(self, ble, name="mpy-uart"):
+        self._ble = ble
+        self._ble.active(True)
+        self._ble.irq(self._irq)
+        ((self._handle_tx, self._handle_rx),) = self._ble.gatts_register_services((_UART_SERVICE,))
+        self._connections = set()
+        self._write_callback = None
+        self._payload = advertising_payload(name=name, services=[_UART_UUID])
+        self._advertise()
+
+    def _irq(self, event, data):
+        # Track connections so we can send notifications.
+        if event == _IRQ_CENTRAL_CONNECT:
+            conn_handle, _, _ = data
+            print("New connection", conn_handle)
+            self._connections.add(conn_handle)
+        elif event == _IRQ_CENTRAL_DISCONNECT:
+            conn_handle, _, _ = data
+            print("Disconnected", conn_handle)
+            self._connections.remove(conn_handle)
+            # Start advertising again to allow a new connection. Alsways connects when there is an available node.
+            self._advertise()
+        elif event == _IRQ_GATTS_WRITE:
+            conn_handle, value_handle = data
+            value = self._ble.gatts_read(value_handle)
+            if value_handle == self._handle_rx and self._write_callback:
+                self._write_callback(value)
+
+    def send(self, data):
+        for conn_handle in self._connections:
+            self._ble.gatts_notify(conn_handle, self._handle_tx, data)
+
+    def is_connected(self):
+        return len(self._connections) > 0
+
+    def _advertise(self, interval_us=500000):
+        print("Starting advertising")
+        self._ble.gap_advertise(interval_us, adv_data=self._payload)
+
+    def on_write(self, callback):
+        self._write_callback = callback
+
+
+def demo1():
+    ble = bluetooth.BLE()
+    p = BLESimplePeripheral(ble)
+
+    def on_rx(v):
+        print("RX", v)
+
+    p.on_write(on_rx)
+
+    i = 0
+    while True:
+        if p.is_connected():
+            # Short burst of queued notifications.
+            for _ in range(3):
+                data = str(i) + "_"
+                print("TX", data)
+                p.send(data)
+                i += 1
+        time.sleep_ms(100)
+        
+def demo2():
+    payload = advertising_payload(
+    name="micropython",
+    services=[bluetooth.UUID(0x181A), bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")],
+    )
+    print(payload)
+    print(decode_name(payload))
+    print(decode_services(payload))
+
+
+if __name__ == "__main__":
     
-    #BLE
-    # Prepare data to send
-    data = {"temperature in C": temperature_celsius, "temperature in F": temperature_fahrenheit}
+    # Create a Bluetooth Low Energy (BLE) object
+    ble = bluetooth.BLE()
 
-    # Convert data to JSON
-    data_json = ujson.dumps(data)
+    # Create an instance of the BLESimplePeripheral class with the BLE object
+    sp = BLESimplePeripheral(ble)
+    adc = ADC(4)
+    # temp_sensor = machine.ADC(machine.ADC.CORE_TEMP)
 
+    while True:
+        if sp.is_connected():  # Check if a BLE connection is established
+            # Read the value from the internal temperature sensor
+            ADC_voltage = adc.read_u16() * (3.3 / (65536))
+            # sensor_value = temp_sensor.read_u16() * (3.3 / (65536))
+            temperature_celcius = 27 - (ADC_voltage - 0.706)/0.001721
+            temp_fahrenheit=32+(1.8*temperature_celcius)
+            # Transmit the temperature value over BLE
+            # print("Temperature in C: {:.2f}°C".format(temperature_celcius))
+            # print("Temperature in F: {:.2f}°F".format(temp_fahrenheit))
+            # print("<...>")
+            temperature = {'temperature_celcius':f"{temperature_celcius}", 'temp_fahrenheit':f"{temp_fahrenheit}"} 
+            temperature_data = str(temperature).encode()
+            sp.send(temperature_data)
+        time.sleep(1)
+    demo1()
+    demo2()
 
